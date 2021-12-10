2d40ab719626495ca90744ba528bd1bb0e2cf8b2
juliajyh <47750603+juliajyh@users.noreply.github.com>
2021-12-09 16:32:39 -0800

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -45,33 +45,33 @@ drawUi = const [ui]
         imageslist = vBox[ str "images", B.hBorder, hBox [ viewport VP1 Vertical $
                       vBox $ str "Names" :
                               str " ":
-                             (str <$> ["Name1", "Name2", "Name3"])
+                             (str <$> ["Name1", "Name2", "Name3", "Name4", "Name5", "Name6", "Name7", "Name8", "Name9"])
                     , B.vBorder
                     , viewport VP2 Vertical $
                       vBox $ str "IDS" :
                               str " ":
-                             (str <$> ["ID1", "ID2", "ID3"])
+                             (str <$> ["ID1", "ID2", "ID3", "ID4", "ID5", "ID6", "ID7", "ID8", "ID9"])
                     , B.vBorder
                     , viewport VP3 Vertical $
                       vBox $ str "ports" :
                               str " ":
-                             (str <$> ["port1", "port2", "port3"])
+                             (str <$> ["port1", "port2", "port3", "port4", "port5", "port6", "port7", "port8", "port9"])
                     ]]
 
         containerslist = vBox[ str "containers", B.hBorder, hBox [ viewport VP4 Vertical $
                       vBox $ str "Names" :
                               str " ":
-                             (str <$> ["Name1", "Name2", "Name3"])
+                             (str <$> ["Name1", "Name2", "Name3", "Name4", "Name5", "Name6", "Name7", "Name8", "Name9"])
                     , B.vBorder
                     , viewport VP5 Vertical $
                       vBox $ str "IDS" :
                               str " ":
-                             (str <$> ["ID1", "ID2", "ID3"])
+                             (str <$> ["ID1", "ID2", "ID3", "ID4", "ID5", "ID6", "ID7", "ID8", "ID9"])
                     , B.vBorder
                     , viewport VP6 Vertical $
                       vBox $ str "ports" :
                               str " ":
-                             (str <$> ["port1", "port2", "port3"])
+                             (str <$> ["port1", "port2", "port3", "port4", "port5", "port6", "port7", "port8", "port9"])
                     ]]
         
 
@@ -100,8 +100,15 @@ appEvent _ (T.VtyEvent (V.EvKey V.KRight [V.MCtrl])) = M.hScrollBy vp3Scroll 1 >
 appEvent _ (T.VtyEvent (V.EvKey V.KLeft  [V.MCtrl])) = M.hScrollBy vp3Scroll (-1) >> M.continue ()
 appEvent _ (T.VtyEvent (V.EvKey V.KDown []))  = M.vScrollBy vp1Scroll 1 >> M.continue ()
 appEvent _ (T.VtyEvent (V.EvKey V.KUp []))    = M.vScrollBy vp1Scroll (-1) >> M.continue ()
-appEvent _ (T.VtyEvent (V.EvKey V.KRight [])) = M.hScrollBy vp2Scroll 1 >> M.continue ()
-appEvent _ (T.VtyEvent (V.EvKey V.KLeft []))  = M.hScrollBy vp2Scroll (-1) >> M.continue ()
+appEvent _ (T.VtyEvent (V.EvKey (V.KChar '1') [])) = M.vScrollBy vp2Scroll 1 >> M.continue ()
+appEvent _ (T.VtyEvent (V.EvKey (V.KChar '1') [V.MCtrl]))  = M.vScrollBy vp2Scroll (-1) >> M.continue ()
+appEvent _ (T.VtyEvent (V.EvKey (V.KChar '2') []))  = M.vScrollBy vp4Scroll 1 >> M.continue ()
+appEvent _ (T.VtyEvent (V.EvKey (V.KChar '2') [V.MCtrl]))    = M.vScrollBy vp4Scroll (-1) >> M.continue ()
+appEvent _ (T.VtyEvent (V.EvKey (V.KChar '3') [])) = M.vScrollBy vp5Scroll 1 >> M.continue ()
+appEvent _ (T.VtyEvent (V.EvKey (V.KChar '3') [V.MCtrl]))  = M.vScrollBy vp5Scroll (-1) >> M.continue ()
+appEvent _ (T.VtyEvent (V.EvKey (V.KChar '4') [])) = M.vScrollBy vp6Scroll 1 >> M.continue ()
+appEvent _ (T.VtyEvent (V.EvKey (V.KChar '4') [V.MCtrl]))  = M.vScrollBy vp6Scroll (-1) >> M.continue ()
+
 appEvent _ (T.VtyEvent (V.EvKey V.KEsc [])) = M.halt ()
 appEvent _ _ = M.continue ()
 
