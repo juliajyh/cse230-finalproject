5afe9da13ae1693a5ce2d10d4f5113d8e71e5c68
Haichao Yang <hcyang@ucsd.edu>
2021-12-09 17:04:01 -0800

    ExecPull

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,7 @@
+{-# LANGUAGE OverloadedStrings #-}
 import TestBrickStates(testBrickStates)
+import TestThemes(testThemes)
+import ExecPull(testExecPull)
 
 main :: IO ()
-main = testBrickStates 
+main = testExecPull
